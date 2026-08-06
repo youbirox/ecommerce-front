57beecabfd255d23a100d5b1ff1cfac3f337923e
Ayoub Moutii <youbirox1994@gmail.com>
2026-08-06 16:43:25 +0200

    fix: configure redux store reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "../features/auth/authSlice";
+
+import productReducer from "../features/products/productsSlice";
+
+import cartReducer from "../features/cart/cartSlice";
+
+import orderReducer from "../features/orders/ordersSlice";
+
 export const store = configureStore({
-    reducer: {},
+  reducer: {
+    auth: authReducer,
+
+    products: productReducer,
+
+    cart: cartReducer,
+
+    orders: orderReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppDispatch = typeof store.dispatch;
