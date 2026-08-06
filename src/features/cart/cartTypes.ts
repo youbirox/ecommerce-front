d8410e4620363a29c857e28104f8ffa8dc82b7b8
Ayoub Moutii <youbirox1994@gmail.com>
2026-08-06 16:24:46 +0200

    feat: implement cart feature

---
+++ src/features/cart/cartTypes.ts
@@ -0,0 +1,11 @@
+import type { Product } from "../products/productsTypes";
+
+export interface CartItem {
+  product: Product;
+
+  quantity: number;
+}
+
+export interface CartState {
+  items: CartItem[];
+}
